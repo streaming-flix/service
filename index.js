bebf9c40efee72a92535c319dfee83f66409ed65
jujulili1 <tyulianto@gmail.com>
2020-09-01 21:21:34 +0700

    add ejs

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//     res.render('pages/home');
-// });
+app.get('/', (req, res) => {
+    res.render('pages/home');
+});
 
 app.use('/api/user', require('./routes/user'));
 app.use('/api/admin', require('./routes/admin'));
